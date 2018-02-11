75f58545f535f7cdd40ca3032d6d52112943a992
Paul Hendrick <paul.g.hendrick@gmail.com>
2018-02-11 03:29:23 -0500

    trim

--- dotfiles-master/scripts/node_setup.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -o errexit -o pipefail -o nounset
-
-echo "Updating npm dependencies"
-npm install -g elm elm-live elm-format gulp gulp-elm
-npm install -g typescript gulp-typescript
--- dotfiles-master/symlinks/bash/.bash_profile
+++
@@ -1,6 +0,0 @@
-if [ -f "$HOME/.bashrc" ]; then
-    source "$HOME/.bashrc"
-fi
-
-### Added by the Heroku Toolbelt
-export PATH="/usr/local/heroku/bin:$PATH"
