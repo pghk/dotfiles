34c97ae2cbc11c928127a55bf61f90d7d27083c2
Paul Hendrick <paul.g.hendrick@gmail.com>
2018-02-17 11:52:24 -0500

    💚 Fix the bash shebangs

--- scripts/get-the-rest.sh
+++ scripts/get-the-rest.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
 # Terminus
 cd ${HOME} && mkdir bin && curl -O https://raw.githubusercontent.com/pantheon-systems/terminus-installer/master/builds/installer.phar && php installer.phar install
