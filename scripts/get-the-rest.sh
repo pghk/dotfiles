673e9c8e4297fb2787c88c46fd5baed8f4d04f9d
Paul Hendrick <paul.g.hendrick@gmail.com>
2018-02-24 12:49:55 -0500

    Get terminus from homebrew instead of self-install

--- scripts/get-the-rest.sh
+++ scripts/get-the-rest.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env sh
 
-# Terminus
-cd ${HOME} && mkdir bin && curl -O https://raw.githubusercontent.com/pantheon-systems/terminus-installer/master/builds/installer.phar && php installer.phar install
-
 # Docksal
 curl -fsSL https://get.docksal.io | sh
 
