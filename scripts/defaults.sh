f5242715f472509597f8240733b0c3e178167314
Paul Hendrick <paul.g.hendrick@gmail.com>
2018-02-23 22:21:54 -0500

    Remove the extra macos defaults script
    Cider can handle these

--- scripts/defaults.sh
+++
@@ -1,6 +0,0 @@
-# Map 'click or tap with two fingers' to the secondary click
-defaults -currentHost write -g com.apple.trackpad.enableSecondaryClick -bool true
-defaults -currentHost write -g com.apple.trackpad.trackpadCornerClickBehavior -int 0
-
-# Set tracking speed
-defaults write -g com.apple.trackpad.scaling 1
