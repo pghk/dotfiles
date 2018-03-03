c82e330184f0d1611310f30734a669490d1f8e61
Paul Hendrick <paul.g.hendrick@gmail.com>
2018-03-03 01:10:37 -0500

    🔪 Pare the cider list way down
    Just get the important stuff first. We'll install the rest of the
    packages after a reboot

--- scripts/get-the-rest.sh
+++ scripts/get-the-rest.sh
@@ -1,5 +1,74 @@
 #!/usr/bin/env sh
 
+casks=(
+  "atom"
+  "backblaze"
+  "bartender"
+  "daisydisk"
+  "deluge"
+  "evernote"
+  "fantastical"
+  "firefox"
+  "flux"
+  "google-chrome"
+  "google-drive-file-stream"
+  "kaleidoscope"
+  "kap"
+  "moom"
+  "numi"
+  "phpstorm"
+  "sequel-pro"
+  "skitch"
+  "slack"
+  "sourcetree"
+  "spotify"
+  "textexpander"
+  "transmit"
+  "visual-studio-code"
+  "xscope"
+  "pallotron-yubiswitch"
+  "font-input"
+  "font-open-sans"
+  "font-roboto"
+  "font-firacode-nerd-font"
+)
+
+formulas=(
+  "terminus"
+  "coreutils"
+  "moreutils"
+  "findutils"
+  "gnu-sed --with-default-names"
+  "wget --with-iri"
+  "gnupg"
+  "grep"
+  "ack"
+  "openssh"
+  "unrar"
+  "rename"
+  "pandoc"
+  "jq"
+  "ssh-copy-id"
+  "tree"
+  "screen"
+  "imagemagick"
+  "gzip"
+  "zopfli"
+  "lftp"
+  "goaccess"
+  "terminal-notifier"
+  "node"
+  "go"
+)
+
+for formula in ${!formulas[@]}; do
+  echo "brew install ${formulas[$formula]}"
+done
+
+for cask in ${!casks[@]}; do
+  echo "brew cask install ${casks[$cask]}"
+done
+
 # Docksal
 curl -fsSL https://get.docksal.io | sh
 
