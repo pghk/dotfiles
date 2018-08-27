1b8adf3fcbf8dbfde1ab5143a26f3448e7ef3c21
Paul Hendrick <paul.g.hendrick@gmail.com>
2018-08-26 22:03:25 -0400

    Update/remove some apps

--- scripts/get-mas-apps.sh
+++ scripts/get-mas-apps.sh
@@ -5,13 +5,12 @@ apps=(
   [1278508951]='Trello'
   [412736166]='piZZa'
   [585829637]='Todoist'
-  [557168941]='Tweetbot'
+  [1384080005]='Tweetbot 3'
   [1091189122]='Bear'
   [918858936]='Airmail 2'
   [867299399]='OmniFocus'
   [924726344]='Deliveries'
   [445189367]='PopClip'
-  [449589707]='Dash'
 )
 
 for ID in ${!apps[@]}; do
