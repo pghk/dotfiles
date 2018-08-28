b161871a5cf309848ad372f0ba90f59d378d129b
Paul Hendrick <paul.g.hendrick@gmail.com>
2018-08-27 20:36:54 -0400

    Download mackup via pip3 instead of homebrew
    https://github.com/lra/mackup/issues/734

--- scripts/after_boot.sh
+++ scripts/after_boot.sh
@@ -5,6 +5,8 @@ if [[ ! -d $logpath ]]; then
   mkdir $logpath
 fi
 
+script $logpath/mackup_install.log sh -x $(pip3 install mackup)
+
 script $logpath/app_store.log sh -x ~/.cider/scripts/stage_2/get-mas-apps.sh
 
 script $logpath/more_homebrew.log sh -x ~/.cider/scripts/stage_2/get-the-rest.sh
