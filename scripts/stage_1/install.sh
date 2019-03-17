5a26d2fc646b1b1b1ff6792b97d68bad25b60380
Paul Hendrick <paul.g.hendrick@gmail.com>
2019-03-17 02:23:39 -0400

    ⬆️ Update Emoji font

--- scripts/stage_1/install.sh
+++ scripts/stage_1/install.sh
@@ -13,7 +13,7 @@ pip install -U --user cider
 ln -s ~/Library/Python/2.7/bin/cider /usr/local/bin/cider
 
 # Replace the emoji font
-curl -L https://github.com/emojione/emojione-assets/releases/download/3.1.2/emojione-apple.ttc\
+curl -L https://github.com/emojione/emojione-assets/releases/download/4.5/emojione-mac.ttc\
  -o "${HOME}/Library/Fonts/Apple Color Emoji.ttc"
 
 # Run Cider to install Homebrew Casks & Formulae, set macOS defaults,  symlink and run additional scripts:
