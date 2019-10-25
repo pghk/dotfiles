b7471274da8556b6ac92ee1b8e82cb81538d77da
Paul Hendrick <paul.g.hendrick@gmail.com>
2019-10-25 00:58:38 -0400

    Try to fix python again

--- scripts/stage_1/install.sh
+++ scripts/stage_1/install.sh
@@ -5,11 +5,16 @@ ruby -e "$(
   curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install
 )"
 
+# Install and setup Python
+brew install pyenv
+pyenv install 3.8.0
+pyenv global 3.8.0
+if command -v pyenv 1>/dev/null 2>&1; then
+  eval "$(pyenv init -)"
+fi
+
 # Install Cider:
 pip install -U --user cider
-
-# Add a symlink for cider
-ln -s ~/Library/Python/2.7/bin/cider /usr/local/bin/cider
 
 # Replace the emoji font
 curl -L https://github.com/emojione/emojione-assets/releases/download/4.5/emojione-mac.ttc\
