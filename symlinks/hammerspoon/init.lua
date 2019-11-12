2dcbca06229d871e428a1612d2e076d0f74de469
Paul Hendrick <paul.g.hendrick@gmail.com>
2019-11-12 09:24:25 -0500

    Add some window size presets to hammerspoon

--- symlinks/hammerspoon/init.lua
+++ symlinks/hammerspoon/init.lua
@@ -11,10 +11,35 @@ hs.hotkey.bind(mash, "f13", function()
     }
   ):send()
 end)
-hs.loadSpoon("AClock")
 
-hs.hotkey.bind(mash, "o", function()
-  spoon.AClock:toggleShow()
+
+
+hs.hotkey.bind(mash, "u", function ()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.w = 4 * (max.w / 7)
+  f.h = 5 * (max.h / 6)
+  f.x = max.x + ((max.w - f.w) / 4)
+  f.y = max.y + ((max.h - f.h) / 2)
+
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind(mash, "o", function ()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.w = 6 * (max.w / 7)
+  f.h = 5 * (max.h / 6)
+  f.x = max.x + ((max.w - f.w) / 2)
+  f.y = max.y + ((max.h - f.h) / 2)
+
+  win:setFrame(f)
 end)
 
 require "grid"
