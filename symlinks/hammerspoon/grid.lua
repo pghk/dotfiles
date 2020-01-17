3bbd9b3ff98479a73c96cbe849256360d6c63b9a
Paul Hendrick <paul.g.hendrick@gmail.com>
2020-01-16 19:01:31 -0500

    [WIP] rework hammerspoon window management

--- symlinks/hammerspoon/grid.lua
+++ symlinks/hammerspoon/grid.lua
@@ -1,10 +1,65 @@
-hs.grid.setGrid'5x3'
-hs.grid.setMargins("8,10")
-hs.window.animationDuration = 0.1
-
-function getWin()
-  return hs.window.focusedWindow()
+function greatestCommonDivisor(a, b)
+  while b ~= 0 do a,b = b,(a % b) end
+  return a
 end
+
+function aspectRatio(x, y)
+  local div = greatestCommonDivisor(x, y)
+  return math.floor(x / div), math.floor(y / div)
+end
+
+function makeGrid(screen)
+  local mode = screen:currentMode()
+  return hs.geometry(nil, nil, aspectRatio(mode.w, mode.h))
+end
+
+function setAllGrids(factor)
+  for key, screen in pairs(hs.screen.allScreens()) do
+    local newGrid = makeGrid(screen)
+    newGrid.w = newGrid.w * factor
+    newGrid.h = newGrid.h * factor
+    hs.grid.setGrid(newGrid, screen)
+    hs.alert.show(' ' .. newGrid.w .. ' x ' .. newGrid.h, screen)
+  end
+end
+
+--[[
+reFrame resizes windows
+
+@param gw grid width, as a divisor of total screen width
+@param gh grid height, as a divisor of total screen height
+
+@param w new width, as a dividend of grid width
+@param h new height, as a dividend of grid height
+
+@param x left padding, as divisor of the remaining horizontal space
+@param y top padding, as divisor of the remaining verital space
+--]]
+function reFrame(new_size, new_grid, margins, position)
+  local win = getWin()
+  local screen = win:screen()
+
+  local current = win:frame()
+  local field = screen:frame()
+
+  local new = {}
+  new.w = field.w * new_size.w / new_grid.x - margins.w
+  new.h = field.h * new_size.h / new_grid.y - margins.h
+  new.x = field.w / (new_grid.x * position.x) + margins.w / 2
+  new.y = field.h / (new_grid.y * position.y) + margins.h / 2 + title_bar_height
+  
+  hs.grid.setMargins(hs.geometry(nil, nil, margins.w, margins.h))
+  hs.grid.setGrid(hs.geometry(nil, nil, new_grid.x, new_grid.y))
+
+  print(hs.geometry(new))
+  return new
+end
+
+hs.hotkey.bind(mash, "z", function() 
+  hs.grid.setGrid('1x1')
+  hs.grid.setMargins('120,0')
+  hs.alert.show('Aspect ratio 4:3')
+end)
 
 --- arrows: move window
 hs.hotkey.bind(mash, "left", function() hs.grid.pushWindowLeft() end)
@@ -31,10 +86,12 @@ hs.hotkey.bind(mash, "7", function() hs.grid.setGrid('7x5'); hs.alert.show('Grid
 hs.hotkey.bind(mash, "/", function() local win = getWin(); win:moveToScreen(win:screen():next()) end)
 
 --- ,: snap window to grid
-hs.hotkey.bind(mash, ",", function() hs.grid.snap(getWin()) end)
+hs.hotkey.bind(mash, "m", function() hs.grid.snap(hs.window.focusedWindow()) end)
 
 --- space: maximize window
-hs.hotkey.bind(mash, "space", function() hs.grid.maximizeWindow() end)
+-- hs.hotkey.bind(mash, "space", function() hs.grid.maximizeWindow() end)
+
+--- note: mash, "." is taken by macos sysdiagnose
 
 --- .: minimize window
 hs.hotkey.bind(mash, ".", function() hs.grid.set(getWin(), '0,0 1x1'); end)
--- symlinks/hammerspoon/init.lua
+++ symlinks/hammerspoon/init.lua
@@ -1,45 +1,8 @@
+hs.logger.defaultLogLevel = 'info'
+title_bar_height = 23
+
 mash = {"shift", "ctrl", "alt"}
 hyper = {"shift", "ctrl", "alt", "cmd"}
-
--- setting grid of each screen based on its aspect ratio
-
-function greatestCommonDivisor(a, b)
-  while b ~= 0 do a,b = b,(a % b) end
-  return a
-end
-
-function calcAspect(x, y)
-  local div = greatestCommonDivisor(x, y)
-  return math.floor(x / div), math.floor(y / div)
-end
-
-function smartGrid(screen)
-  local mode = screen:currentMode()
-  return hs.geometry(nil, nil, calcAspect(mode.w, mode.h))
-end
-
-function setSmartGrids(factor)
-  for key, screen in pairs(hs.screen.allScreens()) do
-    local newGrid = smartGrid(screen)
-    newGrid.w = newGrid.w * factor
-    newGrid.h = newGrid.h * factor
-    hs.grid.setGrid(newGrid, screen)
-    hs.alert.show(' ' .. newGrid.w .. ' x ' .. newGrid.h, screen)
-  end
-end
-
-hs.hotkey.bind(mash, "[", function()
-  setSmartGrids(1)
-end)
-hs.hotkey.bind(mash, "]", function()
-  setSmartGrids(2)
-end)
-hs.hotkey.bind(mash, "\\", function()
-  setSmartGrids(3)
-end)
-
---
-
 
 hs.hotkey.bind(mash, "f13", function()
   hs.reload()
@@ -51,31 +14,41 @@ hs.hotkey.bind(mash, "f13", function()
   ):send()
 end)
 
-function reFrame(w, h, x, y)
-  local win = hs.window.focusedWindow()
-  local screen = win:screen()
-
-  local inner = win:frame()
-  local outer = screen:frame()
-
-  inner.w = outer.w * w
-  inner.h = outer.h * h
-  inner.x = outer.x + ((outer.w - inner.w) / x)
-  inner.y = outer.y + ((outer.h - inner.h) / y)
-
-  return inner
+function getWin()
+  return hs.window.focusedWindow()
 end
 
-hs.hotkey.bind(mash, "u", function ()
-  hs.window.focusedWindow():setFrame(reFrame(0.57, 0.83, 4, 2))
-end)
-
-hs.hotkey.bind(mash, "o", function ()
-  hs.window.focusedWindow():setFrame(reFrame(0.85, 0.833333333, 2, 2))
-end)
-
-hs.hotkey.bind(mash, ";", function ()
-  hs.window.focusedWindow():setFrame(reFrame(0.4375, 0.9375, 32, 2))
-end)
+hs.window.animationDuration = 0.1
 
 require "grid"
+
+-- setting grid of each screen based on its aspect ratio
+default_margins = "8,8"
+hs.grid.setMargins(default_margins)
+setAllGrids(1)
+
+hs.hotkey.bind(mash, "[", function()
+  setAllGrids(1)
+end)
+hs.hotkey.bind(mash, "]", function()
+  setAllGrids(2)
+end)
+hs.hotkey.bind(mash, "\\", function()
+  setAllGrids(.5)
+end)
+
+--
+
+hs.hotkey.bind(mash, "a", function ()
+  local new_size = {w=4, h=3}
+  local new_grid = {x=8, y=5}
+  local margins = {w=(240/8), h=0}
+  local position = {x=1, y=1}
+  getWin():setFrame(reFrame(new_size, new_grid, margins, position))
+end)
+
+
+
+
+
+
