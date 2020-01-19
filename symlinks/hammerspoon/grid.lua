a2cc737c20e04512dda656638d5d940c16d8a01f
Paul Hendrick <paul.g.hendrick@gmail.com>
2020-01-19 13:42:18 -0500

    🔧 Hammerspoon cleanup
    - Adjust window size preset hotkeys
    - Reassign window move-to-next-screen hotkey
    - Remove unused functions
    - Refactor for clarity

--- symlinks/hammerspoon/grid.lua
+++ symlinks/hammerspoon/grid.lua
@@ -23,87 +23,36 @@ function setAllGrids(factor)
   end
 end
 
---[[
-reFrame resizes windows
-
-@param gw grid width, as a divisor of total screen width
-@param gh grid height, as a divisor of total screen height
-
-@param w new width, as a dividend of grid width
-@param h new height, as a dividend of grid height
-
-@param x left padding, as divisor of the remaining horizontal space
-@param y top padding, as divisor of the remaining verital space
---]]
-function reFrame(new_size, new_grid, margins, position)
-  local win = getWin()
-  local screen = win:screen()
-
-  local current = win:frame()
-  local field = screen:frame()
-
-  local new = {}
-  new.w = field.w * new_size.w / new_grid.x - margins.w
-  new.h = field.h * new_size.h / new_grid.y - margins.h
-  new.x = field.w / (new_grid.x * position.x) + margins.w / 2
-  new.y = field.h / (new_grid.y * position.y) + margins.h / 2 + title_bar_height
-  
-  hs.grid.setMargins(hs.geometry(nil, nil, margins.w, margins.h))
-  hs.grid.setGrid(hs.geometry(nil, nil, new_grid.x, new_grid.y))
-
-  print(hs.geometry(new))
-  return new
+function resizeWindow(newWidth, newHeight)
+  function newSize(cell)
+    cell.w = newWidth
+    cell.h = newHeight
+  end
+  hs.grid.adjustWindow(newSize, hs.window.focusedWindow())
 end
 
-hs.hotkey.bind(mash, "z", function() 
-  hs.grid.setGrid('1x1')
-  hs.grid.setMargins('120,0')
-  hs.alert.show('Aspect ratio 4:3')
-end)
-
---- arrows: move window
-local windowMovements = {
-  left = function() hs.grid.pushWindowLeft() end,
-  right = function() hs.grid.pushWindowRight() end,
-  up = function() hs.grid.pushWindowUp() end,
-  down = function() hs.grid.pushWindowDown() end
-}
-for key, action in pairs(windowMovements) do
-  hs.hotkey.bind(mash, key, action, nil, action)
+function smartResize(aspect)
+  local factor = hs.grid.getGrid(hs.screen.mainScreen()).w / 8
+  newWidth = math.ceil(aspect.w * factor)
+  newHeight = math.floor(aspect.h * factor)
+  resizeWindow(newWidth, newHeight)
 end
 
---- ikjl: resize window
-local windowSizers = {
-  i = function() hs.grid.resizeWindowShorter() end,
-  k = function() hs.grid.resizeWindowTaller() end,
-  j = function() hs.grid.resizeWindowThinner() end,
-  l = function() hs.grid.resizeWindowWider() end,
-}
-for key, action in pairs(windowSizers) do
-  hs.hotkey.bind(mash, key, action, nil, action)
+function cleverResize()
+  local grid = hs.grid.getGrid(hs.screen.mainScreen())
+  newWidth = grid.h
+  newHeight = grid.w / 2
+  resizeWindow(newWidth, newHeight)
 end
 
---- 234: resize grid
-hs.hotkey.bind(mash, "1", function() hs.grid.setGrid('12x16'); hs.alert.show('Grid set to 12x16'); end)
-hs.hotkey.bind(mash, "2", function() hs.grid.setGrid('2x2'); hs.alert.show('Grid set to 2x2'); end)
-hs.hotkey.bind(mash, "3", function() hs.grid.setGrid('3x2'); hs.alert.show('Grid set to 3x2'); end)
-hs.hotkey.bind(mash, "4", function() hs.grid.setGrid('4x2'); hs.alert.show('Grid set to 4x2'); end)
-hs.hotkey.bind(mash, "5", function() hs.grid.setGrid('5x3'); hs.alert.show('Grid set to 5x3'); end)
-hs.hotkey.bind(mash, "6", function() hs.grid.setGrid('6x4'); hs.alert.show('Grid set to 6x4'); end)
-hs.hotkey.bind(mash, "7", function() hs.grid.setGrid('7x5'); hs.alert.show('Grid set to 7x5'); end)
+function moveWindowToNextScreen()
+  local window = hs.window.focusedWindow()
+  window:moveToScreen(window:screen():next()) 
+end
 
---- /: move window to next screen
-hs.hotkey.bind(mash, "/", function() local win = getWin(); win:moveToScreen(win:screen():next()) end)
-
---- ,: snap window to grid
-hs.hotkey.bind(mash, "m", function() hs.grid.snap(hs.window.focusedWindow()) end)
-
---- space: maximize window
--- hs.hotkey.bind(mash, "space", function() hs.grid.maximizeWindow() end)
-
---- note: hyper, "." is taken by macos sysdiagnose
-
---- .: minimize window
-hs.hotkey.bind(mash, ".", function() hs.grid.set(getWin(), '0,0 1x1'); end)
-
-hs.hotkey.bind(mash, "return", function() hs.grid.show() end)
+-- debugging helper
+ function getWindowInfo()
+  local window = hs.window.focusedWindow()
+  print(hs.grid.getGrid(hs.screen.mainScreen()))
+  print(hs.grid.get(window))
+end
--- symlinks/hammerspoon/init.lua
+++ symlinks/hammerspoon/init.lua
@@ -1,10 +1,13 @@
 hs.logger.defaultLogLevel = 'info'
-title_bar_height = 23
+defaultGridMargins = "8,8"
+hs.window.animationDuration = 0.05
 
+-- modifier keys (note: [ hyper + . ] is reserved for macos sysdiagnose!
 mash = {"shift", "ctrl", "alt"}
 hyper = {"shift", "ctrl", "alt", "cmd"}
 
-hs.hotkey.bind(mash, "f13", function()
+-- hotkey to reload Hammerspoon configuration
+local reloadConfig = function()
   hs.reload()
   hs.notify.new(
     {
@@ -12,42 +15,53 @@ hs.hotkey.bind(mash, "f13", function()
       informativeText = "Config reloaded"
     }
   ):send()
-end)
+end
+hs.hotkey.bind(mash, "f13", function() reloadConfig() end)
 
-function getWin()
-  return hs.window.focusedWindow()
+--[[ 
+
+  *** Window Management ***
+
+]]
+require "grid"
+hs.grid.setMargins(defaultGridMargins) -- set margins between windows
+setAllGrids(1) -- initially set the grid of each screen to its aspect ratio
+
+-- [[ ONCE-PER-KEYPRESS HOTKEYS ]]
+local singleUseActions = {
+  -- preset window sizes:
+  ["."] = function() hs.grid.maximizeWindow() end, -- full screen
+  m = function() cleverResize() end, -- medium
+  [","] = function() smartResize({w=4, h=3}) end, -- small, landscape
+  ["/"] = function() smartResize({w=3, h=4}) end, -- small, portrait
+  -- change window grids:
+  ["["] = function() setAllGrids(1) end, -- default
+  ["]"] = function() setAllGrids(2) end, -- 2x (for centering)
+  -- move window to next screen:
+  ["\\"] = function() moveWindowToNextScreen() end,
+  -- print grid info of current window to console:
+  -- ? = function() getWindowInfo() end
+}
+for key, action in pairs(singleUseActions) do
+  hs.hotkey.bind(mash, key, action)
 end
 
-hs.window.animationDuration = 0.1
-
-require "grid"
-
--- setting grid of each screen based on its aspect ratio
-default_margins = "8,8"
-hs.grid.setMargins(default_margins)
-setAllGrids(1)
-
-hs.hotkey.bind(mash, "[", function()
-  setAllGrids(1)
-end)
-hs.hotkey.bind(mash, "]", function()
-  setAllGrids(2)
-end)
-hs.hotkey.bind(mash, "\\", function()
-  setAllGrids(.5)
-end)
-
---
-
-hs.hotkey.bind(mash, "a", function ()
-  local new_size = {w=4, h=3}
-  local new_grid = {x=8, y=5}
-  local margins = {w=(240/8), h=0}
-  local position = {x=1, y=1}
-  getWin():setFrame(reFrame(new_size, new_grid, margins, position))
-end)
-
-
+-- [[ REPEAT-WHEN-HELD HOTKEYS ]] 
+local repeatableActions = {
+  -- moving windows
+  left = function() hs.grid.pushWindowLeft() end,
+  right = function() hs.grid.pushWindowRight() end,
+  up = function() hs.grid.pushWindowUp() end,
+  down = function() hs.grid.pushWindowDown() end,
+  -- resizing windows
+  i = function() hs.grid.resizeWindowShorter() end,
+  k = function() hs.grid.resizeWindowTaller() end,
+  j = function() hs.grid.resizeWindowThinner() end,
+  l = function() hs.grid.resizeWindowWider() end
+}
+for key, action in pairs(repeatableActions) do
+  hs.hotkey.bind(mash, key, action, nil, action)
+end
 
 
 
