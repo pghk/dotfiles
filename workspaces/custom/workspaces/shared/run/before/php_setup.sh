f3a7421bc5c1cde977d8ba277d3cae15c5228988
Paul Hendrick <paul.g.hendrick@gmail.com>
2020-11-30 02:49:27 -0500

    🐛 Fix php setup

--- workspaces/custom/workspaces/shared/run/before/php_setup.sh
+++ workspaces/custom/workspaces/shared/run/before/php_setup.sh
@@ -1,22 +1,30 @@
 #!/usr/bin/env bash
 set -o errexit -o nounset -o pipefail -o xtrace
 
-# Install Composer
-EXPECTED_SIGNATURE=$(wget -q -O - https://composer.github.io/installer.sig)
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
+install_composer () {
+  EXPECTED_SIGNATURE=$(wget -q -O - https://composer.github.io/installer.sig)
+  php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+  ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
 
-if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
-then
-    >&2 echo 'ERROR: Invalid installer signature'
-    rm composer-setup.php
-    exit 1
+  if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
+  then
+      >&2 echo 'ERROR: Invalid installer signature'
+      rm composer-setup.php
+      exit 1
+  fi
+
+  php composer-setup.php --install-dir=/usr/local/bin/ --filename=composer
+  rm composer-setup.php
+}
+
+if ! command -v composer >/dev/null; then
+  install_composer
 fi
 
-php composer-setup.php --install-dir=/usr/local/bin/ --filename=composer
-rm composer-setup.php
-
-composer global require consolidation/cgr
+if ! command -v cgr >/dev/null; then
+  composer global require consolidation/cgr
+  ln -s "$(composer config -g home)/vendor/bin/cgr" /usr/local/bin/
+fi
 
 cgr pantheon-systems/terminus
 
