eaef4079735b9709726380e469f1d0fab3e53b66
Paul Hendrick <paul.g.hendrick@gmail.com>
2021-12-31 16:36:15 -0500

    🔧 Maintain Popclip settings

--- workspaces/custom/workspaces/shared/symlinks/mackup/Mackup/Library/Application Support/PopClip/Extensions/fwp.popclipext/script.rb
+++
@@ -1,9 +0,0 @@
-		#!/System/Library/Frameworks/Ruby.framework/Versions/Current/usr/bin/ruby
-
-		require 'cgi'
-
-		url = "FMP://firstwave.inresonance.com/FW_PROJECTS.fmp12?script=GoToOrg[ID]&param={query}"
-		query = ENV['POPCLIP_TEXT']
-		url.sub!(/\{query\}/,CGI.escape(query))
-
-		%x{open "#{url}"}
