2afca507aa144a1b7f58966a5f24585c5fafa1e3
Paul Hendrick <paul.g.hendrick@gmail.com>
2022-01-03 01:42:37 -0500

    🐛 Fix issue with application of macOS settings

--- helpers/dump_macos_settings.sh
+++ helpers/dump_macos_settings.sh
@@ -5,14 +5,14 @@
 
 set -o errexit -o nounset -o pipefail -o xtrace
 
-LOG_DIR_PRE=".sysinstall."
-FILE_PRE=$(date +"%F.%H%M%S")
-LOG_PATH=$(find "$HOME" -maxdepth 1 -type d -name "${LOG_DIR_PRE}*")
+LOG_DIR=".setup_logs"
 
-if [ -z "$LOG_PATH" ]; then
+EXISTING_LOG_PATH=$(find "$HOME" -maxdepth 1 -type d -name "$LOG_DIR")
+FILE_PRE=${1:-$(date +"%F.%H%M%S")}
+
+if [ -z "$EXISTING_LOG_PATH" ]; then
   FILE_PRE="initial"
-  LOG_PATH=~/"$LOG_DIR_PRE"$(date +"%F.%H%M%S")
-  mkdir "$LOG_PATH"
+  mkdir ~/$LOG_DIR
 fi
 
-defaults read > "$LOG_PATH"/"$FILE_PRE".defaults
+defaults read > ~/$LOG_DIR/"$FILE_PRE".defaults
