2c23ce24db9c467c9fde1c9a0aad774956654aa4
Paul Hendrick <paul.g.hendrick@gmail.com>
2022-02-05 19:29:03 -0500

    ⬆️  Upgrade PopClip Extensions for Monterey

---
+++ workspaces/init/symlinks/mackup/Mackup/Library/Application Support/PopClip/Extensions/Quotes.popclipext/Config.js
@@ -0,0 +1,48 @@
+define(function () {
+    // the possible quotes styles
+    var styles = [
+        '“…”',
+        '‘…’',
+        '"…"',
+        "'…'",
+        '`…`',
+        '«…»',
+        '‹…›',
+        '»…«',
+        '›…‹',
+        '「…」',
+        '『…』',
+        '„…“',
+        '‚…‘'
+    ];
+    // generate square icon
+    function makeIcon(style) {
+        return "[[" + style[0] + style[2] + "]]";
+    }
+    function makeIdentifier(index) {
+        return "style-" + index;
+    }
+    var extension = {
+        options: styles.map(function (style, index) {
+            return {
+                identifier: makeIdentifier(index),
+                label: style,
+                type: 'boolean',
+                icon: makeIcon(style),
+                defaultValue: !(index > 0)
+            };
+        }),
+        actions: function (selection, context, options) {
+            if (selection.text.length > 0) {
+                return styles.filter(function (style, index) { return options[makeIdentifier(index)]; }).map(function (style, index) {
+                    return {
+                        title: styles[index],
+                        icon: makeIcon(style),
+                        code: function (selection) { return popclip.pasteText(style[0] + selection.text + style[2]); }
+                    };
+                });
+            }
+        }
+    };
+    return extension;
+});
