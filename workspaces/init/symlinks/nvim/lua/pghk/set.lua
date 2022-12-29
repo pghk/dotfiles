0a64a78b2ea595da0c34e9595164009116b3e1e9
Paul Hendrick <paul.g.hendrick@gmail.com>
2022-12-29 00:32:17 -0500

    ✨ Add Neovim configuration

---
+++ workspaces/init/symlinks/nvim/lua/pghk/set.lua
@@ -0,0 +1,27 @@
+local opt = vim.opt
+
+opt.number = true
+opt.relativenumber = true
+
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.smartindent = true
+opt.wrap = false
+
+opt.swapfile = false
+opt.backup = false
+opt.undodir = os.getenv("HOME") .. "/.undodir"
+opt.undofile = true
+
+opt.hlsearch = false
+opt.incsearch = true
+
+opt.termguicolors = true
+
+opt.scrolloff = 8
+opt.signcolumn = "yes"
+opt.isfname:append("@-@")
+opt.colorcolumn = "80"
+opt.updatetime = 100
