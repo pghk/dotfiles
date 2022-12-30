3dc689179cb31ebb0eddc25852fbe7dc12ac1a13
Paul Hendrick <paul.g.hendrick@gmail.com>
2022-12-30 00:18:57 -0500

    ➕ Install PHP

--- workspaces/init/run/before/php_setup.sh
+++ workspaces/init/run/before/php_setup.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit -o nounset -o pipefail -o xtrace
 
+brew install php
+
 install_composer () {
   EXPECTED_SIGNATURE=$(wget -q -O - https://composer.github.io/installer.sig)
   php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
