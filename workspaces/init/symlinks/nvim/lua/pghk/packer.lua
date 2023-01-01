f5ccc7b660e9c2b2b35db398ac0c3b62e7d2a749
Paul Hendrick <paul.hendrick@communitybrands.com>
2022-12-31 21:01:50 -0500

    🔧 NVIM: allow plugins to bootstrap when cloned

--- workspaces/init/symlinks/nvim/lua/pghk/packer.lua
+++ workspaces/init/symlinks/nvim/lua/pghk/packer.lua
@@ -1,7 +1,15 @@
--- This file can be loaded by calling `lua require('plugins'` from your init.vim
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
 
--- Only required if you have packer configured as `opt`
-vim.cmd.packadd('packer.nvim')
+local packer_bootstrap = ensure_packer()
 
 return require('packer').startup(function(use)
     -- This package manager itself
@@ -73,5 +81,8 @@ return require('packer').startup(function(use)
       }
   }
 
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 
 end)
