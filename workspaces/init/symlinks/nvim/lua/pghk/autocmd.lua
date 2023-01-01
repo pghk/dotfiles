a839a2d2930771c105eff41192eb052219d7e3e2
Paul Hendrick <paul.g.hendrick@gmail.com>
2023-01-01 01:09:39 -0500

    🔧 Start git commit messages in insert mode

--- workspaces/init/symlinks/nvim/lua/pghk/autocmd.lua
+++ workspaces/init/symlinks/nvim/lua/pghk/autocmd.lua
@@ -1,17 +1,25 @@
-local api = vim.api
+local group = vim.api.nvim_create_augroup
+local on = vim.api.nvim_create_autocmd
+
+local main = group("main", { clear = true })
 
 -- Highlight on yank
-local yankGrp = api.nvim_create_augroup("YankHighlight", { clear = true })
-api.nvim_create_autocmd("TextYankPost", {
-  command = "silent! lua vim.highlight.on_yank()",
-  group = yankGrp,
+on("TextYankPost", {
+    command = "silent! lua vim.highlight.on_yank()",
+    group = main
 })
-
 
 -- Disable colorcolumn when not necessary 
-api.nvim_create_autocmd("FileType", {
-  pattern = { "netrw", "gitcommit" },
-  command = "set colorcolumn=",
+on("FileType", {
+    pattern = { "netrw", "git*" },
+    command = "setlocal colorcolumn=",
+    group = main
 })
 
+-- Start git commit messages in insert mode
+on("FileType", {
+    pattern = { "gitcommit", "gitrebase" },
+    command = "startinsert | 1",
+    group = main
+})
 
