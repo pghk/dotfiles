6e43cdd7f06f2fd11925e7d3319efacf64e4173c
Paul Hendrick <paul.g.hendrick@gmail.com>
2023-01-15 19:09:28 -0500

    💄 Switch to Material colorscheme in Neovim & Kitty

--- workspaces/init/symlinks/nvim/after/plugin/lualine.lua
+++ workspaces/init/symlinks/nvim/after/plugin/lualine.lua
@@ -1,52 +1,16 @@
-local colors = {
-    rose = '#ebbcba',
-    pine = '#31748f',
-    iris = '#c4a7e7',
-    foam = '#9ccfd8',
-    love = '#eb6f92',
-    gold = '#f6c177',
-    base = '#232136',
-    surface = '#2a273f',
-    overlay = '#393552',
-    muted = '#6e6a86',
-    subtle = '#908caa',
-    text = '#e0def4',
-    highlight_low = '#2a283e',
-    highlight_med = '#44415a',
-    highlight_high = '#56526e',
-}
-
-local function getTheme ()
-    return {
-        normal = {
-            a = { fg = colors.base, bg = colors.rose, gui = 'bold' },
-            b = { fg = colors.subtle, bg = colors.overlay},
-            c = { fg = colors.subtle, bg = colors.surface},
-        },
-        command = { a = { fg = colors.base, bg = colors.gold, gui = 'bold' } },
-        insert = { a = { fg = colors.base, bg = colors.pine, gui = 'bold' } },
-        visual = { a = { fg = colors.base, bg = colors.iris, gui = 'bold' } },
-        terminal = { a = { fg = colors.base, bg = colors.foam, gui = 'bold' } },
-        replace = { a = { fg = colors.base, bg = colors.love, gui = 'bold' } },
-        inactive = {
-            a = { fg = colors.muted, bg = colors.base, gui = 'bold' },
-            b = { fg = colors.muted, bg = colors.base},
-            c = { fg = colors.muted, bg = colors.highlight_med},
-        },
-    }
-end
-
+Icons = require('pghk/ui/icons')
 require('lualine').setup {
   options = {
-    icons_enabled = true,
-    theme = getTheme(),
+    -- icons_enabled = true,
+    theme = require('material.lualine'),
     disabled_filetypes = { "alpha" },
-    component_separators = { left = '', right = '' },
-    section_separators = { left = '', right = '' },
+    component_separators = { left = '', right = '' },
+    section_separators = { left = '', right = '' },
   },
   sections = {
       lualine_a = { 'mode' },
-      lualine_b = { 'branch', 'diff', 'diagnostics' },
+      lualine_b = {
+          { 'branch', icon = Icons.misc.branch }, 'diff', 'diagnostics' },
       lualine_c = { 'filename' },
       lualine_x = {
 	'encoding',
