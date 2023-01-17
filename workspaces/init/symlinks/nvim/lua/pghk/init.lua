7081d714db754f400fb33cd276cbf94f45304f57
Paul Hendrick <paul.g.hendrick@gmail.com>
2023-01-17 00:39:53 -0500

    ⚡️ Neovim: reduce startup time by 27%
    From 250ms with Packer to 180ms with Lazy, as measured with
    `hyperfine "nvim --headless +qa" --warmup 5`

--- workspaces/init/symlinks/nvim/lua/pghk/init.lua
+++ workspaces/init/symlinks/nvim/lua/pghk/init.lua
@@ -1,5 +1,19 @@
 require("pghk.remap")
 require("pghk.set")
 require("pghk.autocmd")
-require("pghk.packer")
 Icons  = require("pghk.ui.icons")
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+return require('lazy').setup("pghk.plugins")
