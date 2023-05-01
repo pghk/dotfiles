5896b9c8914657e94f88eb9e316b57503809151c
Paul Hendrick <paul.hendrick@communitybrands.com>
2023-05-01 12:09:29 -0400

    🔧 Check in NVim config

--- workspaces/init/symlinks/nvim/after/plugin/lsp.lua
+++ workspaces/init/symlinks/nvim/after/plugin/lsp.lua
@@ -31,13 +31,22 @@ lsp.on_attach(function(_, bufnr)
   nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
   nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
   nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-  nmap('<leader>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, '[W]orkspace [L]ist Folders')
+  -- nmap('<leader>wl', function()
+  --   print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  -- end, '[W]orkspace [L]ist Folders')
+
+  -- if client.name == 'eslint' then
+  --   client.server_capabilities.document_formatting = true
+  -- end
 
   -- Create a command `:Format` local to the LSP buffer
   vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-    vim.lsp.buf.format()
+    vim.lsp.buf.format({
+        filter = function(client)
+            return client.name ~= "tsserver"
+        end,
+        bufnr = bufnr,
+    })
   end, { desc = 'Format current buffer with LSP' })
 end)
 
