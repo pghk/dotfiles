daf9b35ffbed123608b41b475cb06d92011aed4f
Paul Hendrick <paul.hendrick@communitybrands.com>
2023-05-01 12:09:29 -0400

    🔧 Check in NVim config

--- workspaces/init/symlinks/nvim/lua/pghk/set.lua
+++ workspaces/init/symlinks/nvim/lua/pghk/set.lua
@@ -3,9 +3,9 @@ local opt = vim.opt
 opt.number = true
 opt.relativenumber = true
 
-opt.tabstop = 4
-opt.softtabstop = 4
-opt.shiftwidth = 4
+opt.tabstop = 2
+opt.softtabstop = 2
+opt.shiftwidth = 2
 opt.expandtab = true
 opt.smartindent = true
 opt.wrap = false
