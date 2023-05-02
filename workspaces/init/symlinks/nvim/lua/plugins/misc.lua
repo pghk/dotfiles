42b3fa2bfdf4bf78acf52f602563ddf9d5343c9a
Paul Hendrick <paul.g.hendrick@gmail.com>
2023-05-01 21:47:42 -0400

    🐛 Kitty - make sure symbols load correctly

--- workspaces/init/symlinks/nvim/lua/plugins/misc.lua
+++ workspaces/init/symlinks/nvim/lua/plugins/misc.lua
@@ -2,6 +2,7 @@ return {
   "mbbill/undotree", -- Undo history vizualizer
   "tpope/vim-fugitive", -- Git integration
   "tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
+  "fladson/vim-kitty", -- Syntax highlighting for Kitty terminal
   { -- Show a colorcolumn when width exceeded
     "m4xshen/smartcolumn.nvim",
     opts = {
