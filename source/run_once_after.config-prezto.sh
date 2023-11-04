be49a64388526978588ee2279829c51b00eda82f
Paul Hendrick <paul.g.hendrick@gmail.com>
2023-11-04 04:57:38 -0400

    🐛 Try again

--- source/run_once_after.config-prezto.sh
+++ source/run_once_after.config-prezto.sh
@@ -4,10 +4,9 @@ set -o errexit -o nounset -o pipefail -o xtrace
 # Link any remaining runcoms that we're not overriding
 
 Z="${ZDOTDIR:-${XDG_CONFIG_HOME:-$HOME/.config}/zsh}"
-P="${Z}/.zprezto"
 
 setopt EXTENDED_GLOB
-for rcfile in "${P}"/runcoms/^README.md(.N); do
+for rcfile in "${Z}"/.zprezto/runcoms/^README.md(.N); do
  [[ -f "${Z}/.${rcfile:t}" ]] || ln -s "$rcfile" "${Z}/.${rcfile:t}"
 done
 
