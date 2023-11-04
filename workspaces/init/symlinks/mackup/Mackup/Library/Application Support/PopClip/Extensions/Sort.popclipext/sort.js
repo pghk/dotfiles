1561de64512d35066629aabe46a523bc514e6508
Paul <paul.g.hendrick@gmail.com>
2023-11-04 04:25:20 -0400

    🎉 Start over

--- workspaces/init/symlinks/mackup/Mackup/Library/Application Support/PopClip/Extensions/Sort.popclipext/sort.js
+++
@@ -1,3 +0,0 @@
-const lines = popclip.input.text.match(/[^\r\n]+/g)
-lines.sort((a, b) => a.localeCompare(b, undefined, { numeric: true, sensitivity: 'base' }))
-popclip.pasteText(lines.join('\n'))
--- workspaces/init/symlinks/mackup/Mackup/Library/Application Support/PopClip/Extensions/UnixTimestamp.popclipext/README.md
+++
@@ -1,2 +0,0 @@
-# unix-timestamp-popclip
-A popclip extension to convert UNIX timestamps to human readable format.
