5467fcec9511cc7bd2c477da117a0be2d68e7540
Paul <paul.g.hendrick@gmail.com>
2023-11-04 04:25:20 -0400

    🎉 Start over

--- workspaces/init/run/after/launchd_prune_downloads.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -o errexit -o nounset -o pipefail -o xtrace
-
-LAUNCH_AGENT=~/Library/LaunchAgents/com."$USER".prune_downloads.plist
-
-cp "$HOME"/.dotfiles/helpers/auto.prune_downloads.plist "$LAUNCH_AGENT"
-sed -i '' "s/USER/$USER/g" "$LAUNCH_AGENT"
-launchctl bootstrap gui/"$(id -u "$USER")" "$LAUNCH_AGENT"
--- workspaces/init/symlinks/nvim/lua/plugins/misc.lua
+++
@@ -1,6 +0,0 @@
-return {
-  "mbbill/undotree", -- Undo history vizualizer
-  "tpope/vim-fugitive", -- Git integration
-  "tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
-  "fladson/vim-kitty", -- Syntax highlighting for Kitty terminal
-}
