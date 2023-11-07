51c10c5b496d2cb8becfd4ac8e9b423ef8b72e8b
Paul Hendrick <paul.g.hendrick@gmail.com>
2023-11-06 22:39:17 -0500

    ✨ Neovim

---
+++ source/private_dot_config/nvim/lua/plugins/misc.lua
@@ -0,0 +1,6 @@
+return {
+  "mbbill/undotree", -- Undo history vizualizer
+  "tpope/vim-fugitive", -- Git integration
+  "tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
+  "fladson/vim-kitty", -- Syntax highlighting for Kitty terminal
+}
