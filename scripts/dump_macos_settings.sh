6779351955722ec526f15d4c18d2c77c8e40369d
Paul Hendrick <paul@hendrick.dev>
2023-11-11 18:11:58 -0500

    👷 Tweak install

--- scripts/dump_macos_settings.sh
+++ scripts/dump_macos_settings.sh
@@ -5,7 +5,7 @@
 
 set -o errexit -o nounset -o pipefail -o xtrace
 
-FILE_NAME=${1:-$(date +"%F.%H%M%S").defaults}
+FILE_NAME=${1:-$(date +"%F.%H%M").defaults}
 FILE_PATH=${2:-$HOME/.local/share/macos}
 
 [[ -d $FILE_PATH ]] || mkdir -p $FILE_PATH
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 set -o errexit -o nounset -o pipefail -o xtrace
 
+LOG_DIR=${HOME}/.local/share/macos
+
+if [ "${1}" != 'recording' ]; then
+	[[ -d $LOG_DIR ]] || mkdir -p $LOG_DIR
+	DATE=$(date +"%F.%H%M")
+	exec script "${LOG_DIR}/dotfile_install.${DATE}.txt" ${0} 'recording'
+fi
+
 MY_USER=pghk
 
 if [ ! command -v brew ] && [ -z $CI ]; then
 	"Homebrew installation required: see https://brew.sh/ for instructions"
+	exit 1
 fi
 
 brew bundle --no-lock --file=/dev/stdin <<-EOF
