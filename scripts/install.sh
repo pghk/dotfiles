5f9a5c42c6f40db898534e0375dbfdf6e8b9af6d
Paul Hendrick <paul@hendrick.dev>
2023-11-11 18:40:47 -0500

    ⚡️ Produce fewer log lines in CI

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,7 +3,7 @@ set -o errexit -o nounset -o pipefail -o xtrace
 
 LOG_DIR=${HOME}/.local/share/macos
 
-if [ "${1:-''}" != 'recording' ]; then
+if [ "${1:-''}" != 'recording' ] && [ ! $CI ]; then
 	[[ -d $LOG_DIR ]] || mkdir -p $LOG_DIR
 	DATE=$(date +"%F.%H%M")
 	exec script "${LOG_DIR}/dotfile_install.${DATE}.txt" ${0} 'recording'
