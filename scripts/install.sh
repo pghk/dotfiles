270d7be2e3d95488041671ab3f2c086314da3835
Paul Hendrick <paul@hendrick.dev>
2023-11-11 19:49:17 -0500

    🐛 More install script fixes

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-set -o errexit -o nounset -o pipefail -o xtrace
+set -o errexit -o pipefail -o xtrace
 
 LOG_DIR=${HOME}/.local/share/macos
 
-if [ "${1:-''}" != 'recording' ] && [ ! $CI ]; then
+if [ "${1}" != 'recording' ] && [ ! $CI ]; then
 	[[ -d $LOG_DIR ]] || mkdir -p $LOG_DIR
 	DATE=$(date +"%F.%H%M")
 	exec script "${LOG_DIR}/dotfile_install.${DATE}.txt" ${0} 'recording'
@@ -11,9 +11,13 @@ fi
 
 MY_USER=pghk
 
-if [ ! command -v brew ] && [ -z $CI ]; then
-	"Homebrew installation required: see https://brew.sh/ for instructions"
-	exit 1
+if [ ! type brew ] &>/dev/null && [ -z $CI ]; then
+	if [ ! -d /opt/homebrew ]; then
+		eval $(/opt/homebrew/bin/brew shellenv)
+	else
+		"Homebrew installation required: see https://brew.sh/ for instructions"
+		exit 1
+	fi
 fi
 
 brew bundle --no-lock --file=/dev/stdin <<-EOF
