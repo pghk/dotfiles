d0b764930064a0db31b7858999fe10384a095d98
Paul Hendrick <paul@hendrick.dev>
2023-12-01 11:21:50 -0500

    🔧 Stop daily nvim plugin update notifications

--- source/private_dot_config/nvim/lua/config/lazy.lua
+++ source/private_dot_config/nvim/lua/config/lazy.lua
@@ -28,8 +28,8 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { missing = true, colorscheme = { "material" } },
-  checker = { enabled = true }, -- automatically check for plugin updates
+  install = ({ missing = true, colorscheme = { "material" } }),
+  checker = { enabled = true, notify = false }, -- automatically check for plugin updates
   performance = {
     rtp = {
       -- disable some rtp plugins
