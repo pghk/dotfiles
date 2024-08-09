8732299b918da7505671f3c1161f306fcc87c095
Paul Hendrick <paul@hendrick.dev>
2024-08-09 17:59:08 -0400

    ➕ Add new Hammerspoon-based window manager

--- source/dot_hammerspoon/hotkeys/binding.lua
+++ source/dot_hammerspoon/hotkeys/binding.lua
@@ -1,3 +1,5 @@
+local modal = require("hotkeys.modal")
+local managed = require("services.window.tiling")
 local window = require("services.window.grid")
 
 --[[
@@ -8,32 +10,34 @@ local window = require("services.window.grid")
 local MEH = { "shift", "ctrl", "alt" }
 local HYPER = { "shift", "ctrl", "alt", "cmd" }
 
--- hs.hotkey.bind(HYPER, "return", modal.cycleLayoutMode)
--- hs.hotkey.bind(MEH, "n", managed.display.focusNext)
--- hs.hotkey.bind(MEH, "m", modal.toggleWindowFloat)
+hs.hotkey.bind(HYPER, "return", modal.cycleLayoutMode)
 hs.hotkey.bind(MEH, "space", require("services.kitty").toggle)
---
--- local tiledWindowActions = {
---   { HYPER, "m", managed.pane.super },
---
---   { HYPER, "y", managed.space.rotate },
---   { HYPER, "u", managed.space.flip },
---   { HYPER, "i", managed.pane.swap },
---   { HYPER, "o", managed.space.reset },
---
---   { MEH, "y", managed.pane.rotate },
---   { MEH, "o", managed.space.balance },
---
---   { MEH, "h", managed.pane.focusWest },
---   { MEH, "j", managed.pane.focusSouth },
---   { MEH, "k", managed.pane.focusNorth },
---   { MEH, "l", managed.pane.focusEast },
--- }
---
--- local stackedWindowActions = {
---   { MEH, "i", managed.stack.focusNext },
---   { MEH, "u", managed.stack.focusPrev },
--- }
+
+local tiledWindowActions = {
+  { MEH, "y", managed.space.west },
+  { MEH, "u", managed.window.makeTaller },
+  { MEH, "i", managed.window.center },
+  { MEH, "o", managed.window.makeWider },
+  { MEH, "p", managed.space.east },
+
+  { MEH, "h", managed.focus.left },
+  { MEH, "j", managed.focus.down },
+  { MEH, "k", managed.focus.up },
+  { MEH, "l", managed.focus.right },
+
+  -- { HYPER, "", managed.window.makeThinner },
+  -- { HYPER, "", managed.window.makeTaller },
+  --
+  { HYPER, "m", managed.window.fullWidth },
+
+  { HYPER, "u", managed.column.push },
+  { HYPER, "i", managed.column.pop },
+
+  { HYPER, "h", managed.window.swap_left },
+  { HYPER, "j", managed.window.swap_down },
+  { HYPER, "k", managed.window.swap_up },
+  { HYPER, "l", managed.window.swap_right },
+}
 
 local floatingWindowActions = {
   { HYPER, "n", window.moveToNextScreen },
@@ -41,8 +45,8 @@ local floatingWindowActions = {
 
   { MEH, "y", window.rotate },
   { MEH, "u", window.shrink },
-  { MEH, "i", window.grow },
-  { MEH, "o", window.center },
+  { MEH, "i", window.center },
+  { MEH, "o", window.grow },
 }
 
 local floatingWindowRepeatActions = {
@@ -57,17 +61,16 @@ local floatingWindowRepeatActions = {
   { HYPER, "l", window.makeWider },
 }
 
-local function bindHotkeys(list, repeating)
+local function bindModalHotkeys(mode, list, repeating)
   for _, v in ipairs(list) do
     if repeating then
-      hs.hotkey.bind(v[1], v[2], v[3], nil, v[3])
+      mode:bind(v[1], v[2], v[3], nil, v[3])
     else
-      hs.hotkey.bind(v[1], v[2], v[3])
+      mode:bind(v[1], v[2], v[3])
     end
   end
 end
 
--- bindModalHotkeys(modal.tile, tiledWindowActions)
-bindHotkeys(floatingWindowActions)
-bindHotkeys(floatingWindowRepeatActions, true)
--- bindModalHotkeys(modal.stack, stackedWindowActions)
+bindModalHotkeys(modal.tile, tiledWindowActions)
+bindModalHotkeys(modal.float, floatingWindowActions)
+bindModalHotkeys(modal.float, floatingWindowRepeatActions, true)
